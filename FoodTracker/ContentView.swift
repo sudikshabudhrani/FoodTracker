f09c470dd3c50d9289c552790969b75cc681ff86
Sudiksha Ajit Lachman <sudikshabudhrani>
2020-02-27 14:45:19 +0700

    First commit

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Sudiksha Ajit Lachman on 11/02/20.
+//  Copyright © 2020 Sudiksha Ajit Lachman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
